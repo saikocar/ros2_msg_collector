a3eaa8545ccfa6b41174746732ed33589e4ff48c
Yugo Takano <149354988+tfnh621@users.noreply.github.com>
2024-06-05 13:25:40 +0900

    inital commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# ros2_msg_collector
+
+メッセージ定義ファイルを含むパッケージだけを抽出するスクリプト
+
+## 使い方
+
+```bash
+python3 main.py <path/to/workspace/src>
+```
+
+### レインボーで使う例
+
+`sftp://192.168.1.21` をマウントしてから下記を実行
+```bash
+python3 main.py '/run/user/1000/gvfs/sftp:host=192.168.1.21/home/sit/autoware_erga_humble2/src'
+```
+
+メッセージをビルドする
+```bash
+cd `ls -dt */ | head -1`
+rm -r src/planning_validator src/tier4_perception_msgs src/control_performance_analysis src/planning_debug_tools src/dummy_perception_publisher src/control_validator src/operation_mode_transition_manager src/static_centerline_optimizer src/vehicle_cmd_gate src/yabloc_particle_filter
+rosdep update
+rosdep install -y --from-paths src --ignore-src --rosdistro $ROS_DISTRO
+colcon build --symlink-install --cmake-args -DCMAKE_BUILD_TYPE=Release
+```
---
+++ main.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+import shutil
+import time
+
+def find_msg_package_directories(root_dir: Path) -> list[Path]:
+    result_dirs = []
+    for xmlpath in root_dir.rglob('package.xml'):
+        if xmlpath.is_file() and any(xmlpath.parent.rglob('*.msg')):
+            result_dirs.append(xmlpath.parent)
+    return result_dirs
+
+
+if __name__ == '__main__':
+    import argparse
+
+    parser = argparse.ArgumentParser(description="Find and copy directories containing package.xml and .msg files.")
+    parser.add_argument("source_directory", type=str, help="The root directory to search in.")
+    args = parser.parse_args()
+
+    source_directory = Path(args.source_directory)
+    destination_directory = Path(__file__).resolve().parent / f'ws_{int(time.time())}' / 'src'
+    destination_directory.mkdir(parents=True)
+
+    directories = find_msg_package_directories(source_directory)
+    for directory in directories:
+        print('Found:', directory)
+        shutil.copytree(directory, destination_directory / directory.name)
